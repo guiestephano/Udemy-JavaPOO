003d0390414f6b1eea105067bc44140d002b1ffb
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-03-14 15:39:51 -0300

    Projeto DAO: implementando findByDepartment()

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -8,6 +8,8 @@ import model.entities.Seller;
 
 import java.sql.Connection;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Program {
@@ -15,7 +17,14 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("--------- Test 01: seller findById(): ------------");
         Seller  seller = sellerDao.findById(3);
-
         System.out.println(seller);
+        
+        System.out.println("\n--------- Test 02: seller findByDepartment(): ------------");
+        List<Seller> list = new ArrayList<>();
+        Department dep = new Department(2,null);
+        list = sellerDao.findByDepartment(dep);
+        list.forEach(System.out::println);
+
+
     }
 }
