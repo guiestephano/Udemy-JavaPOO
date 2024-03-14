67a54175eb73203dc60cfd97daca39250d1bf45a
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-03-13 21:57:33 -0300

    Projeto DAO: Criando a classe Seller

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,12 +2,16 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+
 
 public class Program {
     public static void main(String[] args){
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21,"Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
